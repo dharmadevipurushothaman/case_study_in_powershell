7909720a5076146464a35852f42d9ee23386b79e
dharmadevipurushothaman <dharmadevi123cool@gmail.com>
2018-12-09 14:04:32 +0000

    modify the status report

--- status.ps1
+++ status.ps1
@@ -1,1 +1,16 @@
-﻿get-service| out-file C:\users\Administrator\Desktop\case.html 
+﻿$html=@()
+$service=get-service
+$html+="<html><head>service status report</head><br><br><body>"
+$html+="<table border=5><tr><th>service_name</th><th>display_name</th><th>status</th><tr>" 
+foreach($services in $service){
+ if($services.status -eq 'running')
+ {
+  $html+="<tr style=color:green><td>$($services.name)</td><td>$($services.displayname)</td><td>$($services.status)</td></tr>"
+
+
+ }
+
+
+}
+$html+="</table></body></html>"
+$html|out-file -FilePath C:\users\Administrator\Desktop\status.html
